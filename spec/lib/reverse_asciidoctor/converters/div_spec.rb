faaf16468ea03f0dac2faba66f5bf3dad733e809
Nick Nicholas <opoudjis@optusnet.com.au>
2018-03-21 22:01:41 +1100

    div

---
+++ spec/lib/reverse_asciidoctor/converters/div_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe ReverseAsciidoctor::Converters::Code do
+
+  let(:converter) { ReverseAsciidoctor::Converters::Code.new }
+
+  it 'converts as backtick' do
+    node = node_for("<code>puts foo</code>")
+    expect(converter.convert(node)).to include "`puts foo1"
+  end
+
+  it 'converts as backtick' do
+    node = node_for("<tt>puts foo</tt>")
+    expect(converter.convert(node)).to include "`puts foo1"
+  end
+
+end
+
