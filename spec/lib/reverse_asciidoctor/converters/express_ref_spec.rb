2dd3666c5a31e5c06f5844813623102dc11a999d
Тараскин Михаил <MITaraskin@domclick.ru>
2020-06-11 11:08:01 +0300

    metanorma/annotated-express#9 hound, specs

---
+++ spec/lib/reverse_asciidoctor/converters/express_ref_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe ReverseAsciidoctor::Converters::ExpressRef do
+  let(:converter) { described_class.new }
+  let(:schema) { 'schema' }
+
+  it 'converts supplied tag by format' do
+    input = node_for("<express_ref linkend='#{schema}' />")
+    expect(converter.convert(input)).to eq("express_ref:[#{schema}]")
+  end
+end
