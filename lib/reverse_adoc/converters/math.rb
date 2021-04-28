b33b26ab461442f8d420d3e853281fb6a3e5a40b
Sebastian Skałacki <skalee@gmail.com>
2021-04-28 04:23:48 +0200

    Extract helper methods in <math> converter

--- lib/reverse_adoc/converters/math.rb
+++ lib/reverse_adoc/converters/math.rb
@@ -9,9 +9,32 @@ module ReverseAdoc
     class Math < Base
       def convert(node, state = {})
         stem = node.to_s.gsub(/\n/, " ")
-        stem = MathML2AsciiMath.m2a(stem) if ReverseAdoc.config.mathml2asciimath
-        stem = stem.gsub(/\[/, "\\[").gsub(/\]/, "\\]").gsub(/\(\(([^\)]+)\)\)/, "(\\1)") unless stem.nil?
+        stem = translate_expression(stem)
+        stem = escape_square_brackets(stem)
+        stem = remove_redundant_round_brackets(stem)
         " stem:[#{stem}] "
+      end
+
+      # Translates MathML to AsciiMath, if that's enabled.
+      def translate_expression(str)
+        if ReverseAdoc.config.mathml2asciimath
+          MathML2AsciiMath.m2a(str)
+        else
+          str
+        end
+      end
+
+      # Precedes square brackets with a +\+.
+      def escape_square_brackets(str)
+        str.gsub(/\[/, "\\[").gsub(/\]/, "\\]") unless str.nil?
+      end
+
+      # Removes reundant round brackets like in +((...))+.
+      #
+      # For some short discussion, see:
+      # https://github.com/metanorma/reverse_adoc/issues/67
+      def remove_redundant_round_brackets(str)
+        str.gsub(/\(\(([^\)]+)\)\)/, "(\\1)") unless str.nil?
       end
     end
 
